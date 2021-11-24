5f39c041df942ecf819d65b9ac3aef629d8c50b1
Polina Maltseva <polina_maltseva@epam.com>
2021-11-25 00:23:38 +0300

    add test files

--- test.py
+++ test.py
@@ -3,7 +3,11 @@ from rotor import *
 from reflector import *
 from enigma import *
 
-encoded_text = enigma("Attitude is a little thing that makes a BIG difference.", 1, 1, 2, 3)
+with open("pairs", "r") as f:
+	pairs = f.read()
+with open("text.txt", "r") as f:
+	text = f.read()
+encoded_text = enigma(text, 1, 1, 0, 2, 0, 3, 0, pairs)
 path = "enigma_result_" + strftime("%Y-%m-%d_%H-%M-%S", gmtime()) + ".txt"
 f = open(path, "w")
 f.write(encoded_text)
