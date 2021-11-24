e0dcc05eee857a07bee301845562445c3f0809c9
Polina Maltseva <polina_maltseva@epam.com>
2021-11-24 22:04:13 +0300

    encoder v1

---
+++ enigma_v1.py
@@ -0,0 +1,6 @@
+def enigma(text, ref, rot1, rot2, rot3):
+    l = ''
+    txt = ''.join([c for c in text.upper() if c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'])
+    for c in txt:
+        l = l + rotor(rotor(rotor(reflector(rotor(rotor(rotor(c, rot3), rot2), rot1), ref), rot1, True), rot2, True), rot3, True)
+    return l
